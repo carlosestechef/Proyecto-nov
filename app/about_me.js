61d18927cc5365eca399d27d954bbf5b2c1b140e
Carlos <carlosestechef@gmail.com>
2024-11-11 09:33:17 -0300

    Primer avance del proyecto, todo a modo de ejemplo

---
+++ app/about_me.js
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+    return (
+        <section>
+            <h1>Mi historia con la pintura</h1>
+            <p>En este proyecto se revela el viaje de cada pintura, mostrando su metamorfosis con un trabajo arduo y mínimamente cuidadoso. <br></br>Un espacio donde el lienzo cobra vida y cada trazo se convierte en una historia de transformación.</p>
+        </section>
+    )
+}
+
+export default AboutMe;
